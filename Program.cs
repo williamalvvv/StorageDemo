baa7e748fbdbee2c590a2237917f2809b6e60035
William Alvarado <william.alvaradoc@outlook.com>
2021-02-10 15:57:08 -0600

    File Upload and Container Deletion are now done

--- Program.cs
+++ Program.cs
@@ -35,30 +35,68 @@ namespace BlobConsoleDemo
             //Get the ConnectionString securely and prints in terminal
             string getConnectionString = config["ConnectionString"];
             string getStudentID = config["StudentID"];
-            Console.WriteLine(getConnectionString);
 
            //Settings for new container creation
             BlobServiceClient myBlobServiceClient = new BlobServiceClient(getConnectionString);
             
-            //Settings for new container name 
+            //Settings for new container name it takes StudentID from appsettings and joins with some other data
             string containerName = "asn-" + getStudentID + "-" + Guid.NewGuid().ToString();
 
             //Create the container and return an container client object
             BlobContainerClient myContainerClient = myBlobServiceClient.CreateBlobContainer(containerName);
+            
             //Minimal validation for Blob container creation
             //DoubleCheck for Creation
             myContainerClient.CreateIfNotExists();
+            
             //Set the container access Type to blobContainer
             myContainerClient.SetAccessPolicy(accessType: PublicAccessType.Blob);
+            Console.WriteLine("New Blob Container Created at: " + myContainerClient.Uri + "for Account: " + myContainerClient.AccountName + "\n\n");
+            Console.WriteLine("Wanna check the results? Go to your azure Account Service Dashboard");
+            Console.WriteLine("Wannna Proced with File Upload to our Fresh New Blob Container: " + myContainerClient.Name + "? Press Y/N");
+            char userResponse = Char.Parse(Console.ReadLine());
 
-            //Logic for Blob Upload
-           string localFilePath = "Azure-Migrate.svg";
-           BlobClient myBlobClient = myContainerClient.GetBlobClient(localFilePath);
-           Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", myBlobClient.Uri);
-           // Open the file and upload its data
-            using FileStream uploadFileStream = File.OpenRead(localFilePath);
-            myBlobClient.Upload(uploadFileStream, true);
-            uploadFileStream.Close();
+            //Conditional Statement for File Upload to a Blob Container
+            if(userResponse=='y' || userResponse=='Y')
+            {
+                //Logic for Blob Upload
+                string localFilePath = "Azure-Migrate.svg";
+                BlobClient myBlobClient = myContainerClient.GetBlobClient(localFilePath);
+                Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", myBlobClient.Uri);
+
+                // Open the file and upload its data
+                using FileStream uploadFileStream = File.OpenRead(localFilePath);
+                myBlobClient.Upload(uploadFileStream, true);
+                uploadFileStream.Close();
+
+                //logic for Listing items into a container
+                Console.WriteLine("Listing blobs container contents...\n\n");
+
+                // List all blobs in the container
+                foreach (BlobItem blobItem in myContainerClient.GetBlobs())
+                {
+                    Console.WriteLine("\t" + blobItem.Name);
+                }
+                Console.WriteLine("Wannna Proced to Delete to our Fresh New Blob Container: " + myContainerClient.Name + "? Press Y/N");
+                userResponse = Char.Parse(Console.ReadLine());
+
+                //Conditional Statement for File Upload to a Blob Container
+                if(userResponse=='y' || userResponse=='Y')
+                {
+
+                    Console.Write("Press any key to begin clean up");
+                    Console.ReadLine();
+                    Console.WriteLine("Deleting blob container...");
+
+                    //Sets deletion for this container
+                    myContainerClient.Delete();
+                    Console.WriteLine("Done");
+                }
+            }
+            else
+            {
+                Console.WriteLine("fine, bye");
+            }
         }
     }
 }
