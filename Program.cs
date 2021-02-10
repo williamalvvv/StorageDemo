593a8722c5faf95adc0fe49ef3cb1f2147492f30
William Alvarado <william.alvaradoc@outlook.com>
2021-02-10 14:52:04 -0600

    Create Operation and Permissions are now Fully Functional

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+//Post build references to use NuGet packages
+/*
+The following packages must be added before building the project 
+
+dotnet add package Microsoft.Extensions.Configuration --version 5.0.0
+dotnet add package Microsoft.Extensions.Configuration.FileExtensions --version 5.0.0
+dotnet add package Microsoft.Extensions.Configuration.Json --version 5.0.0
+dotnet add package Azure.Storage.Blobs --version 12.8.0
+*/
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace BlobConsoleDemo
 {
@@ -9,16 +22,32 @@ namespace BlobConsoleDemo
     {
         static void Main(string[] args)
         {
+            //Settings for protect the connection string used to manage storage account
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json");
 
+            //Set configuration for read access to appsettings.json where ConnectionString is storaged
             IConfiguration config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", true, true)
             .Build();
 
+            //Get the ConnectionString securely and prints in terminal
             string getConnectionString = config["ConnectionString"];
+            string getStudentID = config["StudentID"];
             Console.WriteLine(getConnectionString);
+
+           //Settings for new container creation
+            BlobServiceClient myBlobServiceClient = new BlobServiceClient(getConnectionString);
+            
+            //Settings for new container name 
+            string containerName = "asn" + getStudentID + Guid.NewGuid().ToString();
+
+            //Create the container and return an container client object
+            BlobContainerClient myContainerClient = myBlobServiceClient.CreateBlobContainer(containerName);
+            myContainerClient.CreateIfNotExists();
+            //Set the container access Type to blobContainer
+            myContainerClient.SetAccessPolicy(accessType: PublicAccessType.BlobContainer);
         }
     }
 }
